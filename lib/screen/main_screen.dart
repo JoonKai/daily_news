7bdb56d923d9b6746096701cb96815124d9d240a
손준석 <joonseok831@gmail.com>
2024-05-01 23:48:13 +0900

    1111

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,6 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
@@ -13,10 +17,16 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   List<dynamic> lstNewsInfo = [];
+  //admob
+  late String admobBanenrId; // 하단 배너 id
+  late BannerAd _bannerAd;
+
   @override
   void initState() {
     super.initState();
     getNewsInfo();
+    //admob 세팅
+    setAdmob();
   }
 
   @override
@@ -33,86 +43,100 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ),
       ),
-      body: ListView.builder(
-        itemCount: lstNewsInfo.length,
-        itemBuilder: (context, index) {
-          var newsItem = lstNewsInfo[index];
-          return GestureDetector(
-            child: Container(
-              margin: const EdgeInsets.all(16),
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  //이미지
-                  SizedBox(
-                    height: 170,
-                    width: double.infinity,
-                    child: newsItem['urlToImage'] != null
-                        ? ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              newsItem['urlToImage'],
-                              fit: BoxFit.cover,
-                            ),
-                          )
-                        : ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.asset('assets/noimage.png'),
-                          ),
-                  ),
-                  //반투명 검정 UI
-                  Container(
-                    width: double.infinity,
-                    height: 57,
-                    decoration: ShapeDecoration(
-                      color: Colors.black.withOpacity(0.7),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: lstNewsInfo.length,
+              itemBuilder: (context, index) {
+                var newsItem = lstNewsInfo[index];
+                return GestureDetector(
+                  child: Container(
+                    margin: const EdgeInsets.all(16),
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        //이미지
+                        SizedBox(
+                          height: 170,
+                          width: double.infinity,
+                          child: newsItem['urlToImage'] != null
+                              ? ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    newsItem['urlToImage'],
+                                    fit: BoxFit.cover,
+                                  ),
+                                )
+                              : ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.asset('assets/noimage.png'),
+                                ),
                         ),
-                      ),
-                    ),
-                    child: Container(
-                      margin: const EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            newsItem['title'],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(
-                            height: 6,
-                          ),
-                          Align(
-                            alignment: Alignment.bottomRight,
-                            child: Text(
-                              formatDate(newsItem['publishedAt']),
-                              style: const TextStyle(
-                                  color: Colors.white, fontSize: 10),
+                        //반투명 검정 UI
+                        Container(
+                          width: double.infinity,
+                          height: 57,
+                          decoration: ShapeDecoration(
+                            color: Colors.black.withOpacity(0.7),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              ),
                             ),
                           ),
-                        ],
-                      ),
+                          child: Container(
+                            margin: const EdgeInsets.all(8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  newsItem['title'],
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(
+                                  height: 6,
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Text(
+                                    formatDate(newsItem['publishedAt']),
+                                    style: const TextStyle(
+                                        color: Colors.white, fontSize: 10),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        ///하얀색 뉴스 제목 , 일자 텍스트
+                      ],
                     ),
                   ),
-
-                  ///하얀색 뉴스 제목 , 일자 텍스트
-                ],
-              ),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/detail',
+                        arguments: newsItem);
+                  },
+                );
+              },
             ),
-            onTap: () {
-              Navigator.pushNamed(context, '/detail', arguments: newsItem);
-            },
-          );
-        },
+          ),
+          SizedBox(
+            width: _bannerAd.size.width.toDouble(),
+            height: _bannerAd.size.height.toDouble(),
+            child: AdWidget(
+              ad: _bannerAd,
+            ),
+          )
+        ],
       ),
     );
   }
@@ -146,5 +170,45 @@ class _MainScreenState extends State<MainScreen> {
     } catch (error) {
       print(error);
     }
+  }
+
+  String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      //android
+      if (kReleaseMode) {
+        return 'ca-app-pub-2635737019072788/2076448434'; //release unit-id
+      } else {
+        return 'ca-app-pub-3940256099942544/9214589741'; //debug (test) unit-id
+      }
+    } else if (Platform.isIOS) {
+      //ios
+      if (kReleaseMode) {
+        return ''; //release unit-id
+      } else {
+        return ''; //debug (test) unit-id
+      }
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  void setAdmob() {
+    // admob 세팅
+    _bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: bannerAdUnitId,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            //광고 로드가 완료됨.
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          print(error.message);
+        },
+      ),
+      request: AdRequest(),
+    );
+    _bannerAd.load();
   }
 }
